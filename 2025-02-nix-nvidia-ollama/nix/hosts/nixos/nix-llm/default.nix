bce12c4cdec8dead03efeab43810a89af74ee4b4
rbuelund <mail@rbuelund.dk>
2025-11-19 13:27:16 +0100

    Update default.nix

--- 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -21,12 +21,12 @@
     interfaces.ens18 = {
       useDHCP = false;
       ipv4.addresses = [{
-        address = "10.42.37.100";
+        address = "192.168.100.12";
         prefixLength = 24;
       }];
     };
-    defaultGateway = "10.42.37.254";
-    nameservers = [ "10.42.37.254" ];
+    defaultGateway = "192.168.100.1";
+    nameservers = [ "8.8.8.8" ];
   };
 
   # System localization
